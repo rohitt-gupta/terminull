a4f3fb48737a9b3874cd862290f1a140d58f3514
rohitGupta2022 <pta.rohit28@gmail.com>
2023-07-12 17:42:16 +0530

    adding global and header styles

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<div>
+			<div>@2023 Terminull. All rights reserved.</div>
+			<div></div>
+		</div>
+	);
+}
+
+export default Footer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import React from "react";
+
+const links = [
+	{
+		id: 1,
+		title: "Home",
+		url: "/",
+	},
+	{
+		id: 2,
+		title: "Portfolio",
+		url: "/portfolio",
+	},
+	{
+		id: 3,
+		title: "Blog",
+		url: "/blog",
+	},
+	{
+		id: 4,
+		title: "About",
+		url: "/about",
+	},
+	{
+		id: 5,
+		title: "Contact",
+		url: "/contact",
+	},
+	{
+		id: 6,
+		title: "Dashboard",
+		url: "/dashboard",
+	},
+];
+
+function Navbar() {
+	return (
+		<div className=' h-[90px] flex justify-between items-center'>
+			<Link href='/' className=' font-bold text-2xl'>
+				terminu_l
+			</Link>
+			<div className=' flex items-center gap-5'>
+				{links.map((link) => (
+					<Link key={link.id} href={link.url}>
+						{link.title}
+					</Link>
+				))}
+				<button className=' p-[5px] border-none bg-[#53c28b] text-white cursor-pointer rounded'>
+					Logout
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default Navbar;
