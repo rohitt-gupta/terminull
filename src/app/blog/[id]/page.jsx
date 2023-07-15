74ce7f44427a4d2729fd6a7c00264ba302f579fa
rohitGupta2022 <pta.rohit28@gmail.com>
2023-07-15 19:08:05 +0530

    blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,60 @@
+import Image from "next/image";
 import React from "react";
 
 function BlogPost() {
-	return <div>BlogPost</div>;
+	return (
+		<div className='contai_ner'>
+			<div className='flex'>
+				<div className='flex-1 flex flex-col justify-between'>
+					<h1 className='text-[40px]'>Lorem Ipsum</h1>
+					<p className='text-[18px] font-light'>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+						quidem totam illo reprehenderit sapiente nulla magnam inventore
+						nesciunt eligendi deserunt sit tempore corrupti cumque, culpa
+						voluptates corporis facere? Quos, id.
+					</p>
+					<div className='flex items-center gap-3'>
+						<Image
+							src='https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+							alt=''
+							width={40}
+							height={40}
+							className=' object-cover rounded-[50%]'
+						/>
+						<span className=''>John Doe</span>
+					</div>
+				</div>
+				<div className=' flex-1 h-[300px] relative'>
+					<Image
+						src='https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+						alt=''
+						fill={true}
+						className='object-cover'
+					/>
+				</div>
+			</div>
+			<div className=' mt-[50px] text-xl font-light text-[#999] text-justify'>
+				<p className=''>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus
+					consequuntur laboriosam beatae totam qui omnis vero voluptates ut
+					maiores, repellendus maxime animi architecto necessitatibus aperiam
+					repellat excepturi! Accusantium, soluta excepturi?
+					<br />
+					<br />
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+					fugit possimus. Libero nihil quia, eveniet beatae ipsam iure eum
+					minima earum corporis accusamus modi animi voluptatibus itaque
+					provident facere sunt?
+					<br />
+					<br />
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+					iste ipsa eligendi dolores, incidunt perferendis ipsam nemo corrupti.
+					A est non laudantium deleniti. Nihil quod nobis repudiandae, eos in
+					minima.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Blog() {
-	return <div>Blog</div>;
+	return (
+		<div className='conti_ner'>
+			<Link
+				href='/blog/testId'
+				className='flex items-center gap-[50px] mb-[50px]'
+			>
+				<div className='imgCOntainer'>
+					<Image
+						src='https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+						alt=''
+						width={400}
+						height={250}
+						className='object-cover'
+					/>
+				</div>
+				<div className='content'>
+					<h1 className='mb-[10px]'>Test</h1>
+					<p className='text-xl text-[#999]'>Description</p>
+				</div>
+			</Link>
+			<Link
+				href='/blog/testId'
+				className='flex items-center gap-[50px] mb-[50px]'
+			>
+				<div className='imgCOntainer'>
+					<Image
+						src='https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+						alt=''
+						width={400}
+						height={250}
+						className='object-cover'
+					/>
+				</div>
+				<div className='content'>
+					<h1 className='mb-[10px]'>Test</h1>
+					<p className='text-xl text-[#999]'>Description</p>
+				</div>
+			</Link>
+			<Link
+				href='/blog/testId'
+				className='flex items-center gap-[50px] mb-[50px]'
+			>
+				<div className='imgCOntainer'>
+					<Image
+						src='https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+						alt=''
+						width={400}
+						height={250}
+						className='object-cover'
+					/>
+				</div>
+				<div className='content'>
+					<h1 className='mb-[10px]'>Test</h1>
+					<p className='text-xl text-[#999]'>Description</p>
+				</div>
+			</Link>
+		</div>
+	);
 }
 
 export default Blog;
