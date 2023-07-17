043f1d4a23cfc3d57c5f6682dd2afb26b93086e4
rohitGupta2022 <pta.rohit28@gmail.com>
2023-07-17 18:19:06 +0530

    google auth setup

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,7 +1,18 @@
+"use client";
+import { signIn } from "next-auth/react";
 import React from "react";
 
 function Login() {
-	return <div>Login</div>;
+	return (
+		<div>
+			<button
+				onClick={() => signIn("google")}
+				className=' p-3 bg-gray-400 text-black rounded-md'
+			>
+				SIgn in with google
+			</button>
+		</div>
+	);
 }
 
 export default Login;
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
-// import { ThemeProvider } from "../context/ThemeContext";
+import { SessionProvider } from "next-auth/react";
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,13 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body className={inter.className}>
 				<ThemeProvider>
-					<div className='container'>
-						<Navbar />
-						{children}
-						<Footer />
-					</div>
+					<AuthProvider>
+						<div className='container'>
+							<Navbar />
+							{children}
+							<Footer />
+						</div>
+					</AuthProvider>
 				</ThemeProvider>
 			</body>
 		</html>
